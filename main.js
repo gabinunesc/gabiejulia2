4f24be404a59635094587553a9022fc93a2632a7
Gabriela <143152400+gabinunesc@users.noreply.github.com>
2024-09-24 12:24:20 -0300

    jkaa

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ function soma(number1,number2){
 } 
 console.log(soma(1,2));
 
-function areaQuadrado(lado){
-    return lado*lado;
+function areaQuadrado(lado)
+{ return lado*lado;
 
 }
 let areaTrapezio=function(teste1,teste2,teste3){
     return (teste1+teste2)*teste3/2 
+
 }
 console.log(areaTrapezio(1,2,3));
 
