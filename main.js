d00408e70f19460540ac69eb8a59c7050b728533
Gabriela <143152400+gabinunesc@users.noreply.github.com>
2024-09-24 12:19:29 -0300

    jka

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ console.log(soma(1,2));
 
 function areaQuadrado(lado){
     return lado*lado;
+
 }
-let areaTrapezio=function(teste1,teste2,teste3) {
+let areaTrapezio=function(teste1,teste2,teste3){
     return (teste1+teste2)*teste3/2 
 }
 console.log(areaTrapezio(1,2,3));
+
+function valores 
